016e291566ff62bb06e6afe8b2d33ca510f46fed
Avanimitra <30976071+Avanimitra@users.noreply.github.com>
2017-08-26 11:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool
+var pool= require('pg').pool;
+
 var app = express();
 app.use(morgan('combined'));
-
+var config = {
+    user: 'avanimitrapple',
+    database : 'avanimitrapple',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new pool(config);
+app.get('/test-db', function(req,res) {
+    pool.query('SELECT * FROM article' function(err, result) {
+    if(err) {
+        res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.tostringify(result));
+        }
+    }
+});
+    
 });
 app.get('/article-one', function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
